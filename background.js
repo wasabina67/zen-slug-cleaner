65986eb18463e98c5106c0bbcc1a09f2e510b48d
wasabina67 <wasabina67@gmail.com>
2025-06-20 00:35:01 +0900

    Update background.js

--- background.js
+++ background.js
@@ -7,5 +7,12 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "zen-slug-cleaner" && tab.id) {}
+  if (info.menuItemId === "zen-slug-cleaner" && tab.id) {
+    if (tab.url && tab.url.startsWith("chrome://")) return;
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: () => {
+      },
+    }).catch(err => console.error(err.message));
+  }
 });
