5a8ebfafdc65685eb3ea5b6a098bd2bf082110e2
wasabina67 <wasabina67@gmail.com>
2025-06-25 18:17:54 +0900

    Update tests/run.js

--- tests/run.js
+++ tests/run.js
@@ -1,5 +1,23 @@
 
 function cleanSlug(url) {
+  try {
+    const urlObj = new URL(url);
+    const pathSegments = urlObj.pathname.split('/').filter(segment => segment);
+    // Check the last path segment for the number-SEO-slug pattern
+    if (pathSegments.length > 0) {
+      const lastSegment = pathSegments[pathSegments.length - 1];
+      const match = lastSegment.match(/^(\d+)-(.+)$/);
+      if (match) {
+        pathSegments[pathSegments.length - 1] = match[1];
+      }
+    }
+    urlObj.pathname = '/' + pathSegments.join('/');
+
+    return urlObj.toString();
+  } catch (error) {
+    console.error('URL parsing error:', error);
+    return url;
+  }
 }
 
 function assert(condition, message) {
