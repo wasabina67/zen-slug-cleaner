6c56575e3d7f8a37eec2753c1bc38930b5f81caa
wasabina67 <wasabina67@gmail.com>
2025-06-28 13:43:51 +0900

    Update tests/run.js

--- tests/run.js
+++ tests/run.js
@@ -1,25 +1,4 @@
-
-// cleanSlug needs to be defined inline, so it's copied and extracted to run.js
-function cleanSlug(url) {
-  try {
-    const urlObj = new URL(url);
-    const pathSegments = urlObj.pathname.split('/').filter(segment => segment);
-    // Check the last path segment for the number-SEO-slug pattern
-    if (pathSegments.length > 0) {
-      const lastSegment = pathSegments[pathSegments.length - 1];
-      const match = lastSegment.match(/^(\d+)-(.+)$/);
-      if (match) {
-        pathSegments[pathSegments.length - 1] = match[1];
-      }
-    }
-    urlObj.pathname = '/' + pathSegments.join('/');
-
-    return urlObj.toString();
-  } catch (error) {
-    console.error('URL parsing error:', error);
-    return url;
-  }
-}
+const { cleanSlug } = require('../utils/cleanSlug.js');
 
 function assert(condition, message) {
   if (!condition) {
